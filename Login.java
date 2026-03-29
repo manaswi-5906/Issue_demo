48dad49e433cd79187041beb16ce4e15fe111282
manaswi-5906 <manaswi5906@gmail.com>
2026-03-29 11:05:49 +0530

    Java login program
    Fix typo in password variable name and add newline at end of file.

---
+++ Login.java
@@ -0,0 +1,11 @@
+public class Login{
+     public static void main(String[]args){
+           String username="manaswi";
+           String password="1234";
+           if(username.equals("manaswi")&&pasword.equals("1234")){
+               System.out.println("Login sucessful");
+           }
+           else
+              System.out.println("Login failed")
+                }
+}
